445c0bbcb044895b10e688cb810ab6651f9d64ca
albertofanini-prog <albertofanini@outlook.com>
2022-02-28 17:01:52 +0100

    Chatlist statica

--- script.js
+++ script.js
@@ -4,8 +4,16 @@ const app = new Vue({
     el: '#app',
     data:{
         myPic: 'zio.jpeg',
+        grandpaPic: 'nonno.jpeg',
+        anonymousPic:'anonimo.jpeg',
         myName: 'Alberto',
+        grandpaName: 'Nonno',
+        anonymousName: '+39 349 2525956',
+        grandpaMessage:'Tutto fatto.',
+        anonymousMessage: 'Mi piacerebbe, ma devo andare a fare la spesa.',
+        date:'28/02/2022 12:07',
         advTitle: 'Ricevi notifiche di nuovi utenti',
-        advText: 'Attiva notifiche desktop'
+        advText: 'Attiva notifiche desktop',
+
     }
 })
